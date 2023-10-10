15d365baedd04685042a1b102c0f33c70947034f
gawsalya <rj_gawsalya@hotmail.co.uk>
2023-10-10 17:49:09 +0100

    drafts

---
+++ src/test/java/org/fruit/MainTest.java
@@ -0,0 +1,13 @@
+package org.fruit;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+    @Test
+    void name() {
+    }
+}
